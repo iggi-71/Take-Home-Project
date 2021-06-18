f2bab7d29ae7c31d452f4952d4a1222bb30ba754
iggi-71 <jimenez72iggi@gmail.com>
2021-06-18 12:43:45 -0700

    first commit

---
+++ Take_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Take_Project
+//
+//  Created by apple on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
